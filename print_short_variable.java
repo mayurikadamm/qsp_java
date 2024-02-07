c76364ae72ae712227db77360cf75c2e49b11e6a
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-02-07 19:19:48 +0530

    Add files via upload

---
+++ print_short_variable.java
@@ -0,0 +1,13 @@
+ //short variable prog
+
+class print_short_variable{
+public static void main(String psv[]){
+  short variable=20;
+System.out.println(variable);
+}
+}
+
+/*output: 20
+CT:success
+RT:success
+*/
