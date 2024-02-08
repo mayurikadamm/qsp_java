b94bab7f3d4b8be336e01b49677c402297cf22f1
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-02-08 14:23:13 +0530

    Add files via upload

---
+++ print_string_variable.java
@@ -0,0 +1,13 @@
+ //String variable prog
+
+class print_string_variable{
+public static void main(String psv[]){
+  String variable="Mayuri";
+System.out.println(variable);
+}
+}
+
+/*output: Mayuri
+CT:success
+RT:success
+*/
