2a9ddb93384eaae16dbb8ef9118dcc2ba32ff297
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-03-13 22:23:32 +0530

    fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,1 +1,15 @@
-
+class Fibbonacci {
+    public static void main(String[] args) {
+        int n=10;
+        int firstno=0;
+        int secondno=1;
+        
+        for(int i=0;i<n;i++){
+            int result=firstno+secondno;
+            firstno=secondno;
+            secondno=result;
+        
+        System.out.println(result);
+        }
+    }
+}
