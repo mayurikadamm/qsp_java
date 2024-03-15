c4375b1b15e302ee8e474767041c8af8f839db0a
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-03-15 22:36:11 +0530

    reverse.java

--- reverse.java
+++ reverse.java
@@ -1,1 +1,14 @@
+class reverse {
+    public static void main(String[] var) {
+int b=21798;
+int rev=0;
 
+      do{
+        int rem=b%10;
+        rev=rev*10+rem;
+        b=b/10;
+      }while(b>0);
+    System.out.println(rev);
+    }
+ }
+ //output:89712
