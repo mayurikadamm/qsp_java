7470bee58e94fa0785368601ced07b31b7373a4e
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-03-15 22:19:57 +0530

    Primenumber.java

---
+++ Primenumber.java
@@ -0,0 +1,23 @@
+class Primenumber {
+    public static void main(String[] var) {
+int num=4;
+int c=0;
+int i=2; 
+while(i<num){ 
+    if(num%1==0){
+    c++;
+    }
+    i++;
+    }
+
+if(c==0){
+    System.out.println("prime");
+
+}
+else{
+    System.out.println("not prime");
+
+}
+    }
+ }
+ //output:not prime
