9dc66cdcdf992f3c475b43090d2bf5e320e32a8f
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-03-15 23:15:06 +0530

    count.java

---
+++ count.java
@@ -0,0 +1,12 @@
+class count{
+    public static void main(String[] var) {
+int a=2799;
+      int r=0;
+      do{
+r=a%10;
+        System.out.println(r);
+        a=a/10;
+      }while(a>0);
+    }
+}
+//output:4
