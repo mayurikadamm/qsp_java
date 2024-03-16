e178fd4cc1bd1cda93609685d973c08bbbcbcf2c
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-03-16 22:45:05 +0530

    factorial.java

---
+++ factorial.java
@@ -0,0 +1,14 @@
+class factorial {
+    public static void main(String[] args) {
+    int n=6;
+    int fact=1;
+    int i=1;
+    
+    while (n>=i) {
+        fact=fact*i;
+    i++;
+    }
+           System.out.println(fact);
+    }
+}
+  
