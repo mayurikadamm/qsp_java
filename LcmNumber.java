2c34c6b687d84d0598b0db9200b5cb6c9ada288d
Mayuri Kadam <96526082+mayurikadamm@users.noreply.github.com>
2024-03-16 19:17:27 +0530

    LcmNumber.java

---
+++ LcmNumber.java
@@ -0,0 +1,16 @@
+
+public class LcmNumber{
+   public static void main(String[ ] args){
+        int i=25;
+       int n1=2;
+      int n2=25;
+while(n1<i){
+     if(i%n1==0&&i%n2==0){
+        System.out.println("lcm is "+i);
+             break;
+      }
+    i++;
+        }
+           }
+              } 
+//output:lcm is 50
